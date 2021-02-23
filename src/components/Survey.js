a1993137a228225c3646b197adcc1011a4b98f46
Tawnee Harris <tawneeh@icloud.com>
2021-02-23 13:12:14 -0800

    add successful .set to Firestore
    Co-authored-by: Patrick Osten <patrickosten040@gmail.com>
    Co-authored-by: Garrett Brown <gman9mm@live.com>

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { db } from '../firebase';
+import { v4 } from 'uuid';
 
 db.collection("test")
   .get()
@@ -9,13 +10,34 @@ db.collection("test")
   });
 
 function Survey() {
+
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    db.collection("test")
+      .doc(v4())
+      .set({
+        question1: e.target.question1.value,
+        question2: e.target.question2.value,
+        question3: e.target.question3.value,
+      })
+      .then(() => {
+        console.log("submiotted!")
+        // NotificationManager.success("A new survey answer has been added", "Success");
+      })
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={handleFormSubmission}>
         <label>
-          Question 1:
+          Question 1:</label>
         <input type="text" name="question1" />
-        </label>
+        <label>
+          Question 2:</label>
+        <input type="text" name="question2"></input>
+        <label>
+          Question 3:</label>
+        <input type="text" name="question3"></input>
         <input type="submit" value="Submit" />
       </form>
     </>
