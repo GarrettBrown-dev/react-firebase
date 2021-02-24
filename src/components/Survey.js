42b1ec5bfe6911ca585050d104f4ab745461771d
Tawnee Harris <tawneeh@icloud.com>
2021-02-24 15:10:08 -0800

    update Survey component
    Co-authored-by: Patrick Osten <patrickosten040@gmail.com>
    Co-authored-by: Garrett Brown <gman9mm@live.com>

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -9,9 +9,16 @@ db.collection("test")
     console.log(data);
   });
 
+let data = { //maybe get data from firestore here?
+  question1: "what?",
+  question2: "who?",
+  question3: "where?"
+}
+
 function Survey(props) {
-  console.log(props.data)
+  const { onSurveyClick } = props;
   const handleFormSubmission = (e) => {
+    onSurveyClick();
     e.preventDefault();
     db.collection("answers")
       .doc(v4())
@@ -20,24 +27,24 @@ function Survey(props) {
         question2: e.target.question2.value,
         question3: e.target.question3.value,
       })
-      .then(function(message) {
+      .then(function (message) {
         message = "submoitted!";
         console.log(message);
-      }, function(){console.log("Not submoitted!")});
-        // NotificationManager.success("A new survey answer has been added", "Success");
-      }
-  
-    return(
+      }, function () { console.log("Not submoitted!") });
+    // NotificationManager.success("A new survey answer has been added", "Success");
+  }
+
+  return (
     <>
       <form onSubmit={handleFormSubmission}>
         <label>
-          {props.data.question1}</label>
+          {data.question1}</label>
         <input type="text" name="question1" />
         <label>
-          {props.data.question2}</label>
+          {data.question2}</label>
         <input type="text" name="question2"></input>
         <label>
-          {props.data.question3}</label>
+          {data.question3}</label>
         <input type="text" name="question3"></input>
         <input type="submit" value="Submit" />
       </form>
