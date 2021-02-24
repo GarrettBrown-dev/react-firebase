ccb2f1f20ecf93248603cd1cc1ff160e551a6c5b
Tawnee Harris <tawneeh@icloud.com>
2021-02-24 15:11:14 -0800

    comment out unneeded import statement
    Co-authored-by: Patrick Osten <patrickosten040@gmail.com>
    Co-authored-by: Garrett Brown <gman9mm@live.com>

--- src/components/SurveyList.js
+++ src/components/SurveyList.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import Survey from './Survey';
+// import Survey from './Survey';
 import { Link } from 'react-router-dom';
 
-let data = { //maybe get data from firestore here?
-  question1: "what?",
-  question2: "who?",
-  question3: "where?"
-}
-
-function SurveyList(){
-  return(
+function SurveyList() {
+  return (
     <>
       <h1>this is the survey list dude!</h1>
       <Link to="/">Home</Link>
-      <Survey data={data}/>
     </>
   )
 }
