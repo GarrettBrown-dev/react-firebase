97ed2c2bbb4b3c4f64f04c8ab02df5832fd00d63
Tawnee Harris <tawneeh@icloud.com>
2021-02-24 11:18:33 -0800

    add Signin component
    Co-authored-by: Patrick Osten <patrickosten040@gmail.com>
    Co-authored-by: Garrett Brown <gman9mm@live.com>

---
+++ src/components/Signin.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import firebase from "firebase/app";
+
+function Signin() {
+  function doSignUp(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(function() {
+      console.log("Signed Up!")
+    }).catch(function(error) {
+      console.log(error.message);
+    });
+  }
+
+  return (
+    <>
+    <h1>Sign Up!</h1>
+    <form onSubmit={doSignUp}>
+      <input
+      type='text'
+      name='email'
+      placeholder='email' />
+      <input
+      type='password'
+      name='password'
+      placeholder='Password' />
+      <button type='submit'>Sign up</button>
+    </form>
+    </>
+  );
+}
+
+export default Signin;
