2a0a533eab8f80af48772be274474086c793d8d2
Tawnee Harris <tawneeh@icloud.com>
2021-02-24 16:46:24 -0800

    update Header
    Co-authored-by: Patrick Osten <patrickosten040@gmail.com>
    Co-authored-by: Garrett Brown <gman9mm@live.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Menu, Menuitem } from '@material-ui/core'
 
 const Header = () => {
   return (
     <>
       <h1>Welcome to the Land of Surveys!</h1>
-      <Link to="/Signin">Sign in Here</Link><br/>
+      <Link to="/Signin">Sign in Here</Link><br />
       <Link to="/Surveys">Surveys</Link>
     </>
   )
