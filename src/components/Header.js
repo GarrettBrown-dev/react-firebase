936cc86c4a3e3d10612f15d1cccd15cce676f6b3
Tawnee Harris <tawneeh@icloud.com>
2021-02-25 12:01:54 -0800

    add Breadcrumbs
    Co-authored-by: Patrick Osten <patrickosten040@gmail.com>
    Co-authored-by: Garrett Brown <gman9mm@live.com>
    Co-authored-by: Cory Nordenbrock <cordenbrock@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Menu, Menuitem } from '@material-ui/core'
+import { Typography, Breadcrumbs } from '@material-ui/core'
 
 const Header = () => {
   return (
     <>
       <h1>Welcome to the Land of Surveys!</h1>
-      <Link to="/Signin">Sign in Here</Link><br />
-      <Link to="/Surveys">Surveys</Link>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Typography color="textPrimary">Navigation</Typography>
+        <Link color="inherit" to="/Signin">Sign in Here</Link>
+        <Link color="inherit" to="/Surveys">Surveys</Link>
+      </Breadcrumbs>
     </>
   )
 }
