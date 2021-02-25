2e86551764cc949490f5edb5de1139355f9dbe70
Tawnee Harris <tawneeh@icloud.com>
2021-02-25 12:04:38 -0800

    attempt to .get answers from the Firestore
    Co-authored-by: Patrick Osten <patrickosten040@gmail.com>
    Co-authored-by: Garrett Brown <gman9mm@live.com>
    Co-authored-by: Cory Nordenbrock <cordenbrock@gmail.com>

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -3,14 +3,15 @@ import { db } from '../firebase';
 import { v4 } from 'uuid';
 import Button from '@material-ui/core/Button';
 
-db.collection("test")
-  .get()
-  .then(querySnapshot => {
-    const data = querySnapshot.docs.map(doc => doc.data());
-    console.log(data);
-  });
 
-let data = { //maybe get data from firestore here?
+// db.collection("test")
+//   .get()
+//   .then(querySnapshot => {
+//     const data = querySnapshot.docs.map(doc => doc.data());
+//     console.log(data);
+//   });
+
+let seedData = {
   question1: "what?",
   question2: "who?",
   question3: "where?"
@@ -18,6 +19,7 @@ let data = { //maybe get data from firestore here?
 
 function Survey(props) {
   const { onSurveyClick } = props;
+
   const handleFormSubmission = (e) => {
     onSurveyClick();
     e.preventDefault();
@@ -31,26 +33,29 @@ function Survey(props) {
       .then(function (message) {
         message = "submoitted!";
         console.log(message);
+        let answers = db.collection("answers").get()
+        .then(answers=> {return answers;})
+        
       }, function () { console.log("Not submoitted!") });
     // NotificationManager.success("A new survey answer has been added", "Success");
   }
-
 
 
   return (
     <>
       <form onSubmit={handleFormSubmission}>
         <label>
-          {data.question1}</label>
+          {seedData.question1}</label>
         <input type="text" name="question1" />
         <label>
-          {data.question2}</label>
+          {seedData.question2}</label>
         <input type="text" name="question2"></input>
         <label>
-          {data.question3}</label>
+          {seedData.question3}</label>
         <input type="text" name="question3"></input>
         <Button variant="contained" color="primary" id="btn" type="submit" value="Submit">Submit!</Button>
       </form>
+
     </>
   )
 }
